2444b505c365b3060a1b032b61e8886b2a00da3e
Erik Tallang <e.tallang@gmail.com>
2024-12-02 13:35:56 +0100

    chore: start with a css chip

--- src/components/chip/chip.stories.ts
+++ src/components/chip/chip.stories.ts
@@ -1,22 +1,39 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
-import { html } from 'lit-html';
-
-import './chip';
+import { html } from 'lit';
 
 export default {
   title: 'Components/Chip',
 } satisfies Meta;
 
-export const Default: StoryObj = {
-  render: ({ variant }) => html`<cx-chip variant=${variant}>I'm a chip</cx-chip>`,
-  argTypes: {
-    variant: {
-      control: 'radio',
-      options: ['primary', 'secondary'],
-      description: 'Changes the visual appearance of the button',
-    },
-  },
-  args: {
-    variant: 'primary',
-  },
+export const Display: StoryObj = {
+  render: () => html`
+    <div class="cx-chip cx-chip__purple cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__dark-blue cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__blue cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__yellow cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__green cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__red">Chip text</div>
+  `,
+};
+
+export const DisplaySoft: StoryObj = {
+  render: () => html`
+    <div class="cx-chip cx-chip__soft cx-chip__purple cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__soft cx-chip__dark-blue cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__soft cx-chip__blue cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__soft cx-chip__yellow cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__soft cx-chip__green cx-mr-6">Chip text</div>
+<div class="cx-chip cx-chip__soft cx-chip__red">Chip text</div>
+  `,
+};
+
+export const Removable: StoryObj = {
+  render: () => html`
+    <button class="cx-chip cx-chip__purple cx-mr-6">Chip text</button>
+<button class="cx-chip cx-chip__dark-blue cx-mr-6">Chip text</button>
+<button class="cx-chip cx-chip__blue cx-mr-6">Chip text</button>
+<button class="cx-chip cx-chip__yellow cx-mr-6">Chip text</button>
+<button class="cx-chip cx-chip__green cx-mr-6">Chip text</button>
+<button class="cx-chip cx-chip__red">Chip text</button>
+  `,
 };
