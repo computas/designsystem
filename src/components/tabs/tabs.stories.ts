4a0f34381326c850f3ac4f730cbe424897e8a01d
Erik Tallang <e.tallang@gmail.com>
2024-12-03 09:00:58 +0100

    feat(tab-link): add button example

--- src/components/tabs/tabs.stories.ts
+++ src/components/tabs/tabs.stories.ts
@@ -5,10 +5,18 @@ export default {
   title: 'Components/Tabs',
 } satisfies Meta;
 
-export const Primary: StoryObj = {
+export const RegularTabs: StoryObj = {
+  render: () => html`
+    <button class="cx-tab-link">This is an option</button>
+<button class="cx-tab-link cx-tab-link--active">This is another option</button>
+<button class="cx-tab-link">This is the final option</button>
+  `,
+};
+
+export const TabLink: StoryObj = {
   render: () => html`
     <a href="https://designsystem.computas.com" class="cx-tab-link">This is an option</a>
-    <a href="https://designsystem.computas.com" class="cx-tab-link cx-tab-link--active">This is another option</a>
-    <a href="https://designsystem.computas.com" class="cx-tab-link">This is the final option</a>
+<a href="https://designsystem.computas.com" class="cx-tab-link cx-tab-link--active">This is another option</a>
+<a href="https://designsystem.computas.com" class="cx-tab-link">This is the final option</a>
   `,
 };
