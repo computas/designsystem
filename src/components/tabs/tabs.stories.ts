6e02a76fd0cfdc1a622a3fe8b5ec6030b80ce6df
Erik Tallang <e.tallang@gmail.com>
2024-12-03 08:51:06 +0100

    feat(tab-link): update design

--- src/components/tabs/tabs.stories.ts
+++ src/components/tabs/tabs.stories.ts
@@ -8,5 +8,7 @@ export default {
 export const Primary: StoryObj = {
   render: () => html`
     <a href="https://designsystem.computas.com" class="cx-tab-link">This is an option</a>
+    <a href="https://designsystem.computas.com" class="cx-tab-link cx-tab-link--active">This is another option</a>
+    <a href="https://designsystem.computas.com" class="cx-tab-link">This is the final option</a>
   `,
 };
