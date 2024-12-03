c8417ba4ce365bfbfb506fd617242638d743ee57
Erik Tallang <e.tallang@gmail.com>
2024-12-03 13:34:31 +0100

    docs(button): remove example with error icon button

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -9,21 +9,54 @@ export const Primary: StoryObj = {
   render: () => html`
     <button class="cx-btn__primary cx-btn__lg cx-mr-6">Primary button large</button>
 <button class="cx-btn__primary cx-mr-6">Primary button</button>
-<button class="cx-btn__primary cx-btn__sm">Primary button small</button>`,
+<button class="cx-btn__primary cx-btn__sm cx-mr-6">Primary button small</button>
+<br /><br />
+<button class="cx-btn__primary cx-btn__icon cx-btn__lg cx-mr-6">
+  <div style="width: 32px; height: 32px; border: 1px solid black"></div>
+</button>
+<button class="cx-btn__primary cx-btn__icon cx-mr-6">
+  <div style="width: 24px; height: 24px; border: 1px solid black"></div>
+</button>
+<button class="cx-btn__primary cx-btn__icon cx-btn__sm">
+  <div style="width: 16px; height: 16px; border: 1px solid black"></div>
+</button>
+  `,
 };
 
 export const Secondary: StoryObj = {
   render: () => html`
     <button class="cx-btn__secondary cx-btn__lg cx-mr-6">Secondary button large</button>
 <button class="cx-btn__secondary cx-mr-6">Secondary button</button>
-<button class="cx-btn__secondary cx-btn__sm">Secondary button small</button>`,
+<button class="cx-btn__secondary cx-btn__sm">Secondary button small</button>
+<br /><br />
+<button class="cx-btn__secondary cx-btn__icon cx-btn__lg cx-mr-6">
+  <div style="width: 32px; height: 32px; border: 1px solid black"></div>
+</button>
+<button class="cx-btn__secondary cx-btn__icon cx-mr-6">
+  <div style="width: 24px; height: 24px; border: 1px solid black"></div>
+</button>
+<button class="cx-btn__secondary cx-btn__icon cx-btn__sm">
+  <div style="width: 16px; height: 16px; border: 1px solid black"></div>
+</button>
+`,
 };
 
 export const Tertiary: StoryObj = {
   render: () => html`
     <button class="cx-btn__tertiary cx-btn__lg cx-mr-6">Tertiary button large</button>
 <button class="cx-btn__tertiary cx-mr-6">Tertiary button</button>
-<button class="cx-btn__tertiary cx-btn__sm">Tertiary button small</button>`,
+<button class="cx-btn__tertiary cx-btn__sm">Tertiary button small</button>
+<br /><br />
+<button class="cx-btn__tertiary cx-btn__icon cx-btn__lg cx-mr-6">
+  <div style="width: 32px; height: 32px; border: 1px solid black"></div>
+</button>
+<button class="cx-btn__tertiary cx-btn__icon cx-mr-6">
+  <div style="width: 24px; height: 24px; border: 1px solid black"></div>
+</button>
+<button class="cx-btn__tertiary cx-btn__icon cx-btn__sm">
+  <div style="width: 16px; height: 16px; border: 1px solid black"></div>
+</button>
+`,
 };
 
 export const Danger: StoryObj = {
@@ -31,18 +64,4 @@ export const Danger: StoryObj = {
     <button class="cx-btn__danger cx-btn__lg cx-mr-6">Danger button large</button>
 <button class="cx-btn__danger cx-mr-6">Danger button</button>
 <button class="cx-btn__danger cx-btn__sm">Danger button small</button>`,
-};
-
-export const Icon: StoryObj = {
-  render: () => html`
-    <button class="cx-btn__danger cx-btn__lg cx-btn__icon cx-mr-6">
-  <div style="width: 32px; height: 32px; border: 1px solid black"></div>
-</button>
-<button class="cx-btn__danger cx-btn__icon cx-mr-6">
-  <div style="width: 24px; height: 24px; border: 1px solid black"></div>
-</button>
-<button class="cx-btn__danger cx-btn__icon cx-btn__sm">
-  <div style="width: 16px; height: 16px; border: 1px solid black"></div>
-</button>
-  `,
 };
