b576471d08e8610feec19cf635de8dd21be6d6fb
Erik Tallang <e.tallang@gmail.com>
2024-12-12 16:55:34 +0100

    feat: add back react entry points

--- packages/lib/build.ts
+++ packages/lib/build.ts
@@ -2,16 +2,22 @@ import esbuild from 'esbuild';
 import { dtsPlugin } from 'esbuild-plugin-d.ts';
 import litPlugin from 'esbuild-plugin-lit';
 
-import { dependencies } from './package.json';
+import { dependencies, peerDependencies } from './package.json';
 
 esbuild.build({
-  entryPoints: ['components/**/(index|react).ts'],
+  entryPoints: ['components/**/index.ts', 'components/**/react.ts'],
   outdir: 'dist',
   bundle: true,
   sourcemap: true,
-  metafile: true,
   outbase: 'components',
+  allowOverwrite: true,
   format: 'esm',
   plugins: [litPlugin(), dtsPlugin({ tsconfig: 'tsconfig.lib.json' })],
-  external: [...Object.keys(dependencies)],
+
+  /**
+   * The React wrappers import the web component from the index file.
+   * We do not want to bundle the web component into the react entry point,
+   * so we exclude all imports of "./index" from the bundle.
+   */
+  external: [...Object.keys(dependencies), ...Object.keys(peerDependencies), './index*'],
 });
