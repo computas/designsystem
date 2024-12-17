31e1ada29605b528faa04b152b8e24c14e224b62
frideska <skarseth.fride@gmail.com>
2024-12-17 15:22:12 +0100

    feat(links): with icons

--- packages/lib/components/link/link.stories.ts
+++ packages/lib/components/link/link.stories.ts
@@ -8,12 +8,19 @@ export default {
 export const Link: StoryObj = {
   render: () => html`
 <a class="cx-link cx-mr-6" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Medium link</a>
-<a class="cx-link cx-link--small" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Small link</a>
+<a class="cx-link cx-link--sm" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Small link</a>
   `,
 };
 
 export const InlineLink: StoryObj = {
   render: () => html`
 <div class="cx-body-2">Lorem ipsum dolor sit amet <a class="cx-link cx-link--inline" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Inline link</a> consectetur. Justo orci pellentesque magna quis. Lorem ipsum dolor sit amet consectetur. Justo orci pellentesque magna quis.</div>
+  `,
+};
+
+export const IconLink: StoryObj = {
+  render: () => html`
+<a class="cx-link" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Link med ikon <cx-icon name="edit" class="cx-link__icon"></cx-icon></a>
+<a class="cx-link cx-link--sm" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Link med ikon <cx-icon name="edit" class="cx-link__icon"></cx-icon></a>
   `,
 };
