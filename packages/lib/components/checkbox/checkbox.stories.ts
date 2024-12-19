fe8232ad69f9b63dddd48f8d3ac90497f2442620
Erik Tallang <e.tallang@gmail.com>
2024-12-19 21:20:05 +0100

    fix(checkbox): invalid import

--- packages/lib/components/checkbox/checkbox.stories.ts
+++ packages/lib/components/checkbox/checkbox.stories.ts
@@ -1,8 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 
-import './checkbox';
-
 export default {
   title: 'Components/Checkbox',
 } satisfies Meta;
