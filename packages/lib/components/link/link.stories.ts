300149943766f36a98f620d5c8d13c4c5d7838fe
frideska <skarseth.fride@gmail.com>
2025-01-10 10:19:25 +0100

    refactor(link): inline is now default link

--- packages/lib/components/link/link.stories.ts
+++ packages/lib/components/link/link.stories.ts
@@ -11,20 +11,13 @@ export default {
 
 export const Link: StoryObj = {
   render: () => html`
-<a class="cx-link cx-mr-6" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Medium link</a>
-<a class="cx-link cx-link--sm" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Small link</a>
+  <div class="cx-body-2">Lorem ipsum dolor sit amet <a class="cx-link" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">basic link</a> consectetur. Justo orci pellentesque magna quis. Lorem ipsum dolor sit amet consectetur. Justo orci pellentesque magna quis.</div>
   `,
 };
 
-export const InlineLink: StoryObj = {
+export const StandaloneLink: StoryObj = {
   render: () => html`
-<div class="cx-body-2">Lorem ipsum dolor sit amet <a class="cx-link cx-link--inline" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Inline link</a> consectetur. Justo orci pellentesque magna quis. Lorem ipsum dolor sit amet consectetur. Justo orci pellentesque magna quis.</div>
-  `,
-};
-
-export const IconLink: StoryObj = {
-  render: () => html`
-<a class="cx-link" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Link med ikon <cx-icon name="edit"></cx-icon></a>
-<a class="cx-link cx-link--sm" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Link med ikon <cx-icon name="edit"></cx-icon></a>
+<a class="cx-link cx-link--standalone cx-mr-6" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Standalone link</a>
+<a class="cx-link cx-link--standalone cx-mr-6" href="https://designsystem.computas.com/?path=/docs/basics-colors--overview">Standalone link with icon <cx-icon name="edit"></cx-icon></a>
   `,
 };
