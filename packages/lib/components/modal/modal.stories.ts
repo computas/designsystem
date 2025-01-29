a777f6654b040b7864b3738ac384c5f172f3e564
Andreas Williams <awil@computas.com>
2025-01-29 11:50:44 +0100

    feat: render cx-modal instead of two different markups

--- packages/lib/components/modal/modal.stories.ts
+++ packages/lib/components/modal/modal.stories.ts
@@ -7,10 +7,6 @@ export default {
 	title: 'Components/Modal',
 } satisfies Meta;
 
-export const Modal1: StoryObj = {
-	render: () => html` <h1>Modal1</h1>`,
-};
-
-export const Modal2: StoryObj = {
-	render: () => html` <h1>Modal2</h1>`,
+export const Modal: StoryObj = {
+	render: () => html` <cx-modal></cx-modal>`,
 };
