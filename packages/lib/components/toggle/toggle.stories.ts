c21fee5a267ef78b4e805a708bfe1b580a88049b
Mathias Källström <matk@computas.com>
2025-10-21 10:49:46 +0200

    Created component and page

---
+++ packages/lib/components/toggle/toggle.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+
+export default {
+  title: 'Components/Toggle',
+} satisfies Meta;
+
+export const Default: StoryObj = {
+  render: () => html`
+<label class="cx-toggle">
+  <input type="checkbox" />
+  <span class="cx-toggle__slider"></span>
+  Toggle me
+</label>
+  `,
+};
